b92bc4de8da5bde9a96c6e41fa715db6d0d88357
asad <asaduzzaman@lapasar.com>
2025-10-26 21:21:28 +0600

    update origin

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(cors({
         credentials: true,
-        origin:"*",
+        origin: "http://localhost:3000",
         methods: ["GET", "POST", "PUT", "UPDATE", "DELETE","PATCH", "OPTIONS"]
 }))
 
