8c05b3bc8205adadd2279b0d62c1a5cb4d566ea7
asad <asaduzzaman@lapasar.com>
2025-10-26 21:10:21 +0600

    update methods on cors block

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,8 @@ const app = express()
 
 app.use(cors({
         credentials: true,
-        origin: ["http://localhost:5173/"]
+        origin: ["http://localhost:5173/"],
+        methods: ["GET", "POST", "PUT", "UPDATE", "DELETE","PATCH", "OPTIONS"]
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
